35682d29e085244f0484865e823df4a8fe411f96
kevingilfether <kevingilfether@gmail.com>
2017-10-24 11:29:30 -0400

    Added controller for suggestion box model

--- SuggestionBox/Models/SuggestionModel.cs
+++ SuggestionBox/Models/SuggestionModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,15 @@ namespace SuggestionBox.Models
 {
     public class SuggestionModel
     {
+        // Make Properties for the suggestion model
+        //Add a data annotation above ID
+
+        [Key]
+        public int ID { get; set; }
+
+        public string Topic { get; set; }
+
+        public string Comment { get; set; }
+
     }
 }
